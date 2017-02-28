e018036b1208d6c91adb2ab7a586b2ee32fa8f6b
Daniel Webb <daniel.webb@rslqld.org>
2017-02-28 19:31:33 +1000

    Finished day of month bot

--- Favourites/bot_dayofmonth.ps1
+++ Favourites/bot_dayofmonth.ps1
@@ -1,4 +1,18 @@
-$currentRound = Get-Content -Raw -Path E:\Tippy\current_round.json | ConvertFrom-Json
+# Run .\bot_dayofmonth.ps1 -username <username> -password <password>
+
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$username,
+    [Parameter(Mandatory=$true)]
+    [string]$password
+    )
+
+
+$roundEndpoint = "http://bcs-tip.azurewebsites.net/api/round/current?u=$($username)&p=$($password)"
+$tipEndpoint = "http://bcs-tip.azurewebsites.net/api/tips/submit?u=$($username)&p=$($password)"
+
+$currentRound = Invoke-WebRequest -Uri $roundEndpoint | ConvertFrom-Json
+
 $jsonPayload = @{
     "Round"=$currentRound.Round
 }
@@ -9,29 +23,37 @@ $tips = @()
 ForEach($game in $currentRound.Games){
     $dayOfMonth = ([DateTime]$game.KickOff).Day
 
-    $dayOfMonth
-    if(($game.Home.TeamId - $dayOfMonth) -lt ($game.Away.TeamId)){
+    # Calc smallest difference between day of month and teamid and tip accordingly
+    if(($game.Home.TeamId - $dayOfMonth) -lt ($game.Away.TeamId - $dayOfMonth)){
         $teamId = $game.Home.TeamId
+
     }else{
         $teamId = $game.Away.TeamId
+ 
     }
 
+    # Set margin if margin game
     if($game.IsMarginGame){
-        $margin = [math]::Abs($game.Home.TeamId - $dayOfMonth)
+        $margin = [math]::Abs($teamId - $dayOfMonth)
     }else{
         $margin = $null
     }
 
-    
-     $tips += [pscustomobject]@{
+    $tips += [pscustomobject]@{
         "MatchId" = $game.MatchId
         "TeamId" = $teamId
         "Margin" = $margin
-        }
-   
+    }
     
 }
 
 
 $jsonPayload.add("Tips",$tips)
-$jsonPayload | ConvertTo-Json | out-file e:\tippy\my_tips_dom.json
+$response = Invoke-WebRequest -Uri $tipEndpoint -ContentType 'application/json' -Method POST -Body ($jsonPayload | ConvertTo-Json)
+
+if ($response.StatusCode -eq 200){
+    Write-Host -ForegroundColor Green "Tips have been submitted successfully"
+}else{
+    Write-Host -ForegroundColor Red "Tips failed"
+    $response.StatusCode
+}
